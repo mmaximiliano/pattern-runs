a3ff719c9c10310c00f6c7cec21788c79093d819
Maxi Martino <mmartino@medallia.com>
2022-02-07 12:14:30 -0300

    Update pattern_runner.sh

--- pattern_runner.sh
+++ pattern_runner.sh
@@ -2,13 +2,13 @@
 
 echo "----- Running massive pattern creator -----"
 
-c=8
+c=1
 
-for th in 450 470 490 500 520 540
+for th in 450 460 470 490 500 520 540
 do
-  for seed in 1 2 3 4 5 6 7 8
+  for seed in 11
   do
-    echo "Loop: $c/1"
+    echo "Loop: $c/7"
     echo "Running th= $th seed= $seed"
     python3 pattern-run.py -th $th -seed $seed
     echo
